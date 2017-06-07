0bb22ed1e0983f0803e0fa71417d914acc8da27b
Palmer-JC <jeffrey.c.palmer@gmail.com>
2017-06-07 16:08:41 -0400

    actual AbstractGrandEntrance class

---
+++ QueuedInterpolation/src/entrances/AbstractGrandEntrance.ts
@@ -0,0 +1,25 @@
+/// <reference path="../meshes/Mesh.ts"/>
+module QI {
+    export class AbstractGrandEntrance {
+        /**
+         * @constructor - This is the required constructor for a GrandEntrance.  This is what Tower of Babel
+         * generated code expects.
+         * @param {QI.Mesh} _mesh - Root level mesh to display.
+         * @param {Array<number>} durations - The millis of various sections of entrance.
+         * @param {BABYLON.Sound} soundEffect - An optional instance of the sound to play as a part of entrance.
+         * @param {boolean} disposeSound - When true, dispose the sound effect on completion. (Default false)
+         */
+        constructor(public _mesh: QI.Mesh, public durations : Array<number>, public soundEffect? : BABYLON.Sound, disposeSound? : boolean) {
+            if (this.soundEffect && disposeSound) {
+                var ref = this;
+                this.soundEffect.onended = function() {
+                    ref.soundEffect.dispose();
+               }
+            }
+        }
+        
+        public makeEntrance() : void {
+            throw "Must be over-ridden by sub-classes"
+        }
+    }
+}
