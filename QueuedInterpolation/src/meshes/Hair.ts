75f0ddd291c0c2a9737d65192b61dec044f90579
Palmer-JC <jeffrey.c.palmer@gmail.com>
2017-08-14 10:46:27 -0400

    more break out of assemble

--- QueuedInterpolation/src/meshes/Hair.ts
+++ QueuedInterpolation/src/meshes/Hair.ts
@@ -13,6 +13,7 @@ module QI{
         private _strandNumVerts : number[];
         
         // members, so it is easier to develop
+        public namedParameter;
         public seed = 0;
         public colorSpread = 0.05; // The maximum amount to randomly change the color of a thread
         public intraStrandColorSpread = 0.02;
@@ -50,73 +51,104 @@ module QI{
          */
         public assemble(strandNumVerts : number[], rootRelativePositions : number[], longestStrand? : number, stiffness? : number, boneName? : string) : void {
             var idx = 0; // index used for writing into indices
-            var pdx = 0; // index used for writing into positions
             var mdx = 0; // index used for writing into matrix indices & weights
             
-            var indices = [];  // cannot use Uint32Array as it is not worth finding out how big it is going to be in advance
-            var nPosElements = rootRelativePositions.length;
-            var positions32 = new Float32Array(nPosElements);
-            
-            var matrixIndices : Float32Array;
-            var matrixWeights : Float32Array;
-            var deltaStiffness : number;
-            var boneIndex = (this.skeleton && boneName) ? this.skeleton.getBoneIndexByName(boneName): null;
-            if (boneIndex) {
-                matrixIndices = new Float32Array(nPosElements / 3 * 4);
-                matrixWeights = new Float32Array(nPosElements / 3 * 4);
-                deltaStiffness = 1 - stiffness;
-            }
-            
-            var rootX  : number, rootY  : number, rootZ  : number;
-            var deltaX : number, deltaY : number, deltaZ : number;
+            // weights used relative positions, so it must run before toAbsolutePositions()
+            var hasSkeleton = this._assignWeights(strandNumVerts, rootRelativePositions, longestStrand, stiffness, boneName);
+            var absPositions = this._toAsbolutePositions(strandNumVerts, rootRelativePositions);
 
+            var indices = [];  // cannot use Uint32Array as it is not worth finding out how big it is going to be in advance
+            var nPosElements = absPositions.length;
+            var positions32 = new Float32Array(absPositions);
+            
             for (var i = 0, nStrands = strandNumVerts.length; i < nStrands; i++) {
-                rootX = positions32[pdx    ] = rootRelativePositions[pdx    ];
-                rootY = positions32[pdx + 1] = rootRelativePositions[pdx + 1];
-                rootZ = positions32[pdx + 2] = rootRelativePositions[pdx + 2];
-                pdx += 3;
-                
                 idx++;
                 
-                 if (boneIndex) {
-                    matrixIndices[mdx] = boneIndex;
-                    matrixWeights[mdx] = 1;
-                    mdx += 4;
-                }
-                
-                for (var vert = 1; vert < strandNumVerts[i]; vert++) {
-                    deltaX = rootRelativePositions[pdx    ]; positions32[pdx    ] = rootX + deltaX;
-                    deltaY = rootRelativePositions[pdx + 1]; positions32[pdx + 1] = rootY + deltaY;
-                    deltaZ = rootRelativePositions[pdx + 2]; positions32[pdx + 2] = rootZ + deltaZ;
-                    pdx += 3;
-                    
-                   if (boneIndex) {
-                        matrixIndices[mdx] = boneIndex;
-                        matrixWeights[mdx] = .0000010; //1 - (deltaStiffness * (this._lengthSoFar(deltaX, deltaY, deltaZ) / longestStrand));
-                        mdx += 4;
-                    }
-                    
+                for (var vert = 1; vert < strandNumVerts[i]; vert++) {                    
                     indices.push(idx - 1);
                     indices.push(idx);
                     idx++;
                 }
             }
             this.setVerticesData(BABYLON.VertexBuffer.PositionKind, positions32);
-             
-            this.setIndices(indices);
-            if (boneIndex) {
+
+            
+            // cannot write weights until position have, do now
+            if (hasSkeleton) {
                 this.numBoneInfluencers = 1;
-                this.setVerticesData(BABYLON.VertexBuffer.MatricesIndicesKind, matrixIndices);
-                this.setVerticesData(BABYLON.VertexBuffer.MatricesWeightsKind, matrixWeights);
+                this.setVerticesData(BABYLON.VertexBuffer.MatricesIndicesKind, this._matrixIndices);
+                this.setVerticesData(BABYLON.VertexBuffer.MatricesWeightsKind, this._matrixWeights);
             }
+            this.setIndices(indices);
             
             // assign stuff to members to be able to change vertex colors afterward & set for the first time
             this._strandNumVerts = strandNumVerts;
             this._nPosElements = nPosElements;
             this.assignVertexColor();
         }
+        private _matrixIndices : Float32Array;
+        private _matrixWeights : Float32Array;
+        private _assignWeights(strandNumVerts : number[], rootRelativePositions : number[], longestStrand? : number, stiffness? : number, boneName? : string) : boolean {
+            var boneIndex = (this.skeleton && boneName) ? this.skeleton.getBoneIndexByName(boneName): null;
+            if (!boneIndex) return false;
+            
+            var pdx = 0; // index used for reading into positions
+            var mdx = 0; // index used for writing into matrix indices & weights
+            
+            var nPosElements = rootRelativePositions.length;
+            
+            this._matrixIndices = new Float32Array(nPosElements / 3 * 4);
+            this._matrixWeights = new Float32Array(nPosElements / 3 * 4);
+            var deltaStiffness = 1 - stiffness;
+            
+            for (var i = 0, nStrands = strandNumVerts.length; i < nStrands; i++) {
+                this._matrixIndices[mdx] = boneIndex;
+                this._matrixWeights[mdx] = 1;
+                mdx += 4;
+                pdx += 3;
+                
+                for (var vert = 1; vert < strandNumVerts[i]; vert++) {
+                    this._matrixIndices[mdx] = boneIndex;
+                    this._matrixWeights[mdx] = .0000010; //1 - (deltaStiffness * (this._lengthSoFar(rootRelativePositions[pdx], rootRelativePositions[pdx + 1], rootRelativePositions[pdx + 2]) / longestStrand));
+                    mdx += 4;
+                    pdx += 3;
+                 }
+            }
+            return true;
+        }
+        
+        /**
+         * this actually edits the relative positions in line, so returning the same array as input, changed to abs values
+         */
+        private _toAsbolutePositions(strandNumVerts : number[], rootRelativePositions : number[]) : number[] {
+            var pdx = 0; // index used for writing into positions
+            
+            var nPosElements = rootRelativePositions.length;
+            var positions32 = new Float32Array(nPosElements);
+            
+            var rootX  : number, rootY  : number, rootZ  : number;
 
+            for (var i = 0, nStrands = strandNumVerts.length; i < nStrands; i++) {
+                rootX = rootRelativePositions[pdx    ];
+                rootY = rootRelativePositions[pdx + 1];
+                rootZ = rootRelativePositions[pdx + 2];
+                pdx += 3;
+                
+                for (var vert = 1; vert < strandNumVerts[i]; vert++) {
+                    rootRelativePositions[pdx    ] += rootX;
+                    rootRelativePositions[pdx + 1] += rootY;
+                    rootRelativePositions[pdx + 2] += rootZ;
+                    pdx += 3;
+                    
+                }
+            }
+            return rootRelativePositions;
+        }
 
+        private extrudeTriangle(atIdx : number, hasPrev : boolean, hasNext : boolean, absPositions : number[] , positions32 : Float32Array, offset) : void {
+            
+        }
+        
         public assignVertexColor() : void {
             this._workingSeed = this.seed;
             
@@ -156,7 +188,7 @@ module QI{
         private _lengthSoFar(deltaX : number, deltaY : number, deltaZ : number) : number {
             return Math.sqrt( (deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ) );
         }
-        
+                
         // from https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
         private _workingSeed = this.seed;
         public random() : number {
@@ -172,7 +204,7 @@ module QI{
     Hair._Colors["LIGHT_BROWN"    ] = new BABYLON.Color3(0.35,  0.22,  0.09);
     Hair._Colors["DIRTY_BLONDE"   ] = new BABYLON.Color3(0.45,  0.35,  0.15);
     Hair._Colors["BLONDE"         ] = new BABYLON.Color3(0.65,  0.55,  0.28);
-    Hair._Colors["PLATINUM_BLONDE"] = new BABYLON.Color3(0.75,  0.70,  0.35);
+    Hair._Colors["PLATINUM_BLONDE"] = new BABYLON.Color3(0.95,  0.95,  0.625);
     Hair._Colors["RED"            ] = new BABYLON.Color3(0.367, 0.072, 0.026);
     Hair._Colors["ORANGE"         ] = new BABYLON.Color3(0.50,  0.19,  0.03);
     Hair._Colors["WHITE"          ] = new BABYLON.Color3(0.90,  0.90,  0.95);
