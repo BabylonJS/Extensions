df5ade72575571a929bedc9ba639abcba0ac65ae
Pierre Glibert <i.pierreg@gmail.com>
2017-10-21 11:36:16 +0200

    ui

--- gradingSceneOptimizer/ts/gradingSceneOptimizer.ts
+++ gradingSceneOptimizer/ts/gradingSceneOptimizer.ts
@@ -1298,7 +1298,7 @@ module BABYLON {
 
 
         // add css rules
-        var css = '#grades {z-index: 1;position: fixed;background-color: white;padding: 20px;}#grades li {font-family: sans-serif;border-bottom: 1px solid black;padding: 10px 0 10px 0;cursor: pointer;}#grades li:hover {color: gray;}'
+        var css = '#grades {z-index: 1;position: fixed;background-color: white;padding: 20px; top:0; right:0;}#grades li {font-family: sans-serif;border-bottom: 1px solid black;padding: 10px 0 10px 0;cursor: pointer;}#grades li:hover {color: gray;}'
         style.innerText = css;
         document.getElementsByTagName('head')[0].appendChild(style);
         // add container
