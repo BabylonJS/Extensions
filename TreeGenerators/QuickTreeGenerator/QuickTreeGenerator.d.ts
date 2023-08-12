eb0a9add39b1c40a5e59aec9bd329ebc6d9593d9
tbo47 (Thibaut) <thibaut.lassalle@gmail.com>
2023-08-12 04:07:25 -0700

    add typescript conf

---
+++ TreeGenerators/QuickTreeGenerator/QuickTreeGenerator.d.ts
@@ -0,0 +1,2 @@
+import * as BABYLON from '@babylonjs/core';
+export declare const QuickTreeGenerator: (sizeBranch: number, sizeTrunk: number, radius: number, trunkMaterial: BABYLON.Material, leafMaterial: BABYLON.Material, scene?: BABYLON.Scene) => BABYLON.Mesh;
